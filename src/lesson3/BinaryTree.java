1d0f5f81bef1138bafe12bac7b07ee52decd4861
K1unk <klunk@inbox.ru>
2019-10-23 18:46:34 +0300

    ~

--- src/lesson3/BinaryTree.java
+++ src/lesson3/BinaryTree.java
@@ -35,12 +35,10 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
         Node<T> newNode = new Node<>(t);
         if (closest == null) {
             root = newNode;
-        }
-        else if (comparison < 0) {
+        } else if (comparison < 0) {
             assert closest.left == null;
             closest.left = newNode;
-        }
-        else {
+        } else {
             assert closest.right == null;
             closest.right = newNode;
         }
@@ -74,10 +72,350 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
      */
     @Override
     public boolean remove(Object o) {
-        // TODO
-        throw new NotImplementedError();
+        @SuppressWarnings("unchecked")
+        T oInT = (T) o;
+        System.out.println(oInT);
+        System.out.println(height());
+        if (height() == 0) return false;
+        size--;
+
+        try {
+            System.out.print(root.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.println();
+
+        try {
+            System.out.print(root.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.println();
+
+        try {
+            System.out.print(root.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.println();
+
+        try {
+            System.out.print(root.left.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.println();
+
+        try {
+            System.out.print(root.left.left.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.left.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.left.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.left.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.left.right.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.left.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.left.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.left.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.right.left.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.print(" ");
+        try {
+            System.out.print(root.right.right.right.right.value);
+        } catch (Exception e) {
+            System.out.print("_");
+        }
+
+        System.out.println();
+
+        find23(root,oInT);
+        return true;
     }
 
+    private Node<T> find23(Node<T> start,T valueOfNodeThatWillBeRemoved) {
+        if (start==null) return null;
+        int comparison = valueOfNodeThatWillBeRemoved.compareTo(start.value);
+        if (comparison < 0) {
+            start.left = find23(start.left, valueOfNodeThatWillBeRemoved);
+        } else {
+            if (comparison>0) {
+                start.right = find23(start.right, valueOfNodeThatWillBeRemoved);
+            } else {
+                System.out.println("NUUU");
+                if (start.left != null && start.right != null) {
+                    start = minimum(start.right);
+                    start.right = find23(start.right,start.value) ;
+                } else {
+                    if (start.left != null) {
+                        start = start.left;
+                    } else {
+                        start = start.right;
+                    }
+                }
+            }
+        }
+        return start;
+    }
+    private Node<T> minimum(Node<T> start) {
+        if (start.left==null) return start;
+        return minimum(start.left);
+    }
+//    private boolean find22(Node<T> start, T valueOfNodeThatWillBeRemoved) {
+//        int  comparison = valueOfNodeThatWillBeRemoved.compareTo(start.value);
+//        if (comparison == 0) {
+//            System.out.println(start.value);
+//            System.out.println(valueOfNodeThatWillBeRemoved);
+//            System.out.println("ROOT IS sTRATS");
+//            start = null;
+//            return false;
+//        } else if (comparison < 0) {
+//            if (start.left.value == valueOfNodeThatWillBeRemoved) return startToRemove(start,valueOfNodeThatWillBeRemoved);
+//            return find22(start.left, valueOfNodeThatWillBeRemoved);
+//        } else {
+//            if (start.right.value == valueOfNodeThatWillBeRemoved) return startToRemove(start,valueOfNodeThatWillBeRemoved);
+//            return find22(start.right, valueOfNodeThatWillBeRemoved);
+//        }
+//    }
+//    private boolean startToRemove(Node<T> parent, T valueOfNodeThatWillBeRemoved) {
+//        if (parent.left!=null) if (parent.left.value == valueOfNodeThatWillBeRemoved) {
+//            System.out.println("LEF");
+//            if (parent.left.left==null && parent.left.right==null) {
+//                parent.left=null;
+//                System.out.println("leftDeleted,00");
+//
+//                return true;
+//            }
+//            if (parent.left.left==null) {
+//               // parent.left=null;
+//                parent.left=parent.left.right;
+//
+//                System.out.println("leftDeleted,01");
+//
+//                return true;
+//            }
+//            if (parent.left.right==null) {
+//              //  parent.left=null;
+//                parent.left=parent.left.left;
+//
+//                System.out.println("leftDeleted,10");
+//
+//                return true;
+//            }
+//            return deleteRecurse(parent,valueOfNodeThatWillBeRemoved,0);
+//        }
+//
+//        if (parent.right!=null)if (parent.right.value == valueOfNodeThatWillBeRemoved) {
+//            System.out.println("RIG");
+//            if (parent.right.left==null && parent.right.right==null) {
+//                parent.right=null;
+//                System.out.println("rightDeleted,00");
+//
+//                return true;
+//            }
+//            if (parent.right.left==null) {
+//                parent.right=parent.right.right;
+//               // parent.right=null;
+//                System.out.println("rightDeleted,01");
+//
+//                return true;
+//            }
+//            if (parent.right.right==null) {
+//                parent.right=parent.right.left;
+//               // parent.right=null;
+//                System.out.println("rightDeleted,10");
+//
+//                return true;
+//            }
+//            return deleteRecurse(parent,valueOfNodeThatWillBeRemoved,1);
+//        }
+//
+//        return true;
+//    }
+//    private boolean deleteRecurse(Node<T> parent, T valueOfNodeThatWillBeRemoved, int right) {
+//        if (right==0) {
+//            int  comparison = parent.value.compareTo(valueOfNodeThatWillBeRemoved);
+//            if (comparison>0) return deleteRecurse(parent.left,valueOfNodeThatWillBeRemoved,0 );
+//        }
+//        if (right==1) {
+//            int  comparison = parent.value.compareTo(valueOfNodeThatWillBeRemoved);
+//            if (comparison<0) return deleteRecurse(parent.right,valueOfNodeThatWillBeRemoved,0 );
+//        }
+//        System.out.println("DDDOUBLE");
+//       return true;
+//    }
     @Override
     public boolean contains(Object o) {
         @SuppressWarnings("unchecked")
